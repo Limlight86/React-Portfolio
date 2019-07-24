24e5c0d0b0ac30fd96d002a810be9340151898c6
Lazaro <lquin008@gmail.com>
2019-07-24 16:00:14 -0400

    routing and initial folder structure in place

---
+++ src/pages/About/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./About.module.css";
+
+const About = () =>(
+  <div className={styles.about}>
+    <h1>You are on the About page</h1>
+    <p>This page will have a small story about my journey as a dev.</p>
+  </div>
+) 
+
+export default About;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Home.module.css"
+
+const Home = () =>(
+  <div className={styles.home}>
+    <h1>You are on the HOME page</h1>
+    <p>Home Page, make it pretty, highlight what tech I know, show off a couple of featured projects.</p>
+  </div>
+)
+
+export default Home

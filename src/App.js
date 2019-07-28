ae0d3392c5b02c47ab2b9f2d3034b8608c838c02
Lazaro <lquin008@gmail.com>
2019-07-28 13:29:56 -0400

    added context and api calls

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import { Home, About, Projects, Contact } from './pages'
-import { Layout } from './containers'
+import { Home, About, Projects, Contact } from "./pages";
+import { Layout } from "./containers";
+import { PortfolioContextProvider } from "./context/PortfolioContext";
 
 const App = () => (
   <BrowserRouter>
-  <Layout>
-    <Route exact path="/" component={Home} />
-    <Route path="/about" component={About} />
-    <Route path="/projects" component={Projects} />
-    <Route path="/contact" component={Contact} />
-  </Layout>
+    <PortfolioContextProvider>
+      <Layout>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/contact" component={Contact} />
+      </Layout>
+    </PortfolioContextProvider>
   </BrowserRouter>
 );
 

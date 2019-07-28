c45c0c85735318492c20657cd41762f9bc7360f7
Lazaro <lquin008@gmail.com>
2019-07-28 14:38:28 -0400

    api connection working

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "./Home.module.css";
+import { PortfolioContext } from "../../context/PortfolioContext";
 import { Navigation } from "../../components/";
 
-const Home = ({ match }) => (
-  <div className={styles.home}>
-    <Navigation currentPath={match.path} />
-    <h1 className={styles.header}>You are on the HOME page</h1>
-    <p>
-      Home Page, make it pretty, highlight what tech I know, show off a couple
-      of featured projects.
-    </p>
-  </div>
-);
+const Home = ({ match }) => {
+  const { githubData } = useContext(PortfolioContext);
+  const { avatar_url } = githubData;
+  return (
+    <div className={styles.home}>
+      <Navigation currentPath={match.path} />
+      <h1 className={styles.header}>You are on the HOME page</h1>
+      <img src={avatar_url} alt="Limlight86" />
+      <p>
+        Home Page, make it pretty, highlight what tech I know, show off a couple
+        of featured projects.
+      </p>
+    </div>
+  );
+};
 
 export default Home;

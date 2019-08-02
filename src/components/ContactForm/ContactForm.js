0e5f45581a9510174b291fe8636ceb6c21ad582d
Lazaro <lquin008@gmail.com>
2019-08-02 14:41:41 -0400

    form compnent set up

---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './ContactForm.module.css'
+
+const ContactForm = () => (
+  <h1>Contact FORM</h1>
+)
+
+
+
+export default ContactForm
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,12 +1,13 @@
 import React from "react";
 import styles from "./Contact.module.css";
-import { Navigation } from '../../components/'
+import { Navigation, ContactForm } from '../../components/'
 
 const Contact = ({ match }) => (
   <div className={styles.contact}>
     <Navigation currentPath={match.path} />
     <h1 className={styles.header}>You are on the Contact page</h1>
     <p>Simple contact form for sending me a message to my email.</p>
+    <ContactForm/>
   </div>
 ) 
 

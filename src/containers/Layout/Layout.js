b4c1ccf600b577e2f58cc60e57bff91670e9d6ba
Lazaro <lquin008@gmail.com>
2019-08-03 19:12:10 -0400

    side drawer implemented

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "./Layout.module.css";
-import { Backdrop } from "../../components/";
+import { Backdrop, SideDrawer } from "../../components/";
 
 const Layout = ({ children }) => (
   <div className={styles.layout}>
     <Backdrop />
+    <SideDrawer/>
     {children}
   </div>
 );

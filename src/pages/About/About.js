b17b8302683b26f4e44113c23432681ac34437c1
Lazaro <lquin008@gmail.com>
2019-08-03 00:00:55 -0400

    link works to resume

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,12 +1,15 @@
 import React from "react";
-import styles from "./About.module.css";
+import { Link } from "react-router-dom";
 import { Navigation } from '../../components/'
+import styles from "./About.module.css";
+import Resume from '../../assets/Lazaro_Quintana_Resume.pdf'
 
 const About = ({ match }) =>(
   <div className={styles.about}>
     <Navigation currentPath={match.path} />
     <h1 className={styles.header}>You are on the About page</h1>
     <p>This page will have a small story about my journey as a dev.</p>
+    <button><Link to={Resume} target="_blank" download>Download my Resume</Link></button>
   </div>
 ) 
 

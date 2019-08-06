5084e357451c66819265ddee03d8e23d602c70bd
Lazaro <lquin008@gmail.com>
2019-08-05 21:27:12 -0400

    added required fields to contact form

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -5,11 +5,11 @@ import { email } from "../../assets";
 const ContactForm = () => (
     <form action="https://formspree.io/lquin008@gmail.com" method="POST" className={styles.contactForm}>
       <div className={styles.personal}>
-        <input type="text" name="name" autoComplete="off" placeholder="Name" />
-        <input type="email" name="email" autoComplete="off" placeholder="Email" />
+        <input type="text" name="name" autoComplete="off" placeholder="Name" required />
+        <input type="email" name="email" autoComplete="off" placeholder="Email" required />
         <input type="phone" name="phone" autoComplete="off" placeholder="Phone Number" />
       </div>
-      <textarea name="message" placeholder="Message" rows="10" cols="100" className={styles.textArea} />
+      <textarea name="message" placeholder="Message" rows="10" cols="100" className={styles.textArea} required />
       <button className={styles.contactButton} type="submit"><img src={email} alt="send"/></button>
     </form>
 );

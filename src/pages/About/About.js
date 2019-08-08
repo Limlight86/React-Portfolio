8134c11d31db1b6431e6f432315afcd70ba5a388
Lazaro <lquin008@gmail.com>
2019-08-07 21:18:12 -0400

    about hero in place

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -2,19 +2,22 @@ import React from "react";
 import { Navigation, Footer } from "../../components/";
 import styles from "./About.module.css";
 import { ResumeButton, MyStory } from "../../components";
-import { mealFlyWin } from "../../assets";
+
 
 const About = ({ match }) => (
   <div className={styles.about}>
     <Navigation currentPath={match.path} />
-    <h1 className={styles.header}>Meet Laz!</h1>
-    <ResumeButton />
-    <div className={styles.mealFly}>
-      <img src={mealFlyWin} alt="ptich night c33" />
-      <span className={styles.caption}>My team, Meal-Fly, accepting our award for best project on pitch night C33. (Judges on left.)</span>
+    <div className={styles.hero}>
+      <h1 className={styles.header}>Meet Laz!</h1>
+      <span className={styles.caption}>
+        My team, Meal-Fly, accepting our award for best project on pitch night
+        C33. (Judges on left.)
+      </span>
+      <ResumeButton />
     </div>
+    
     <MyStory />
-    <Footer currentPath={match.path}/>
+    <Footer currentPath={match.path} />
   </div>
 );
 
